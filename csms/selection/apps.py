48c218bd2c28ffbbc8f543a7e8504d9b1b627893
Yunde Liu <yundepub@outlook.com>
2021-04-17 15:28:10 +0800

    use verbose name

--- csms/selection/apps.py
+++ csms/selection/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SelectionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'csms.selection'
+    verbose_name = 'Course Selection Management System'

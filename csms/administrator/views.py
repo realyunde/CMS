283e7ff6cd4a43a680bca21247d4532ae1f0cfc4
Yunde Liu <yundepub@outlook.com>
2021-04-18 11:53:01 +0800

    add logout

--- csms/administrator/views.py
+++ csms/administrator/views.py
@@ -4,5 +4,10 @@ from django.shortcuts import render, redirect
 def index(request):
     logged = request.session.get('logged')
     if not logged:
-        redirect('/')
+        return redirect('/')
     return render(request, 'administrator/index.html')
+
+
+def logout(request):
+    request.session.flush()
+    return redirect('/')

7648720d28bff285018957c04fdca6c622bf4851
Yunde Liu <yundepub@outlook.com>
2021-04-19 21:18:01 +0800

    fix error

--- cms/admin/views.py
+++ cms/admin/views.py
@@ -24,11 +24,11 @@ def logout(request):
 
 def school(request):
     keyword = request.GET.get('keyword', '').strip()
-    page = request.GET.get('keyword', 0)
+    page = request.GET.get('page', 1)
     try:
-        page = int(page) - 1
+        page = int(page)
         if page < 0:
-            page = 0
+            page = 1
     except ValueError:
         page = 1
     context = {}
@@ -46,6 +46,18 @@ def school(request):
         if action == 'delete':
             schools = request.POST.getlist('schoolIdList')
             print(schools)
-    # get all schools
-    context['school_list'] = School.objects.filter(name__contains=f'{keyword}')
+    # get school list
+    offset = (page - 1) * 10
+    if len(keyword) == 0:
+        count = int(School.objects.count() / 10) + 1
+        school_list = School.objects.all()[offset:offset + 10]
+    else:
+        count = int(School.objects.filter(name__contains=f'{keyword}').count() / 10) + 1
+        school_list = School.objects.filter(name__contains=f'{keyword}')[offset:offset + 10]
+    context['school_list'] = school_list
+    context['page_now'] = page - 1
+    context['page_prev'] = count if page <= 1 else (page - 1)
+    context['page_next'] = 1 if page >= count else (page + 1)
+    context['page_count'] = range(count)
+    context['page_keyword'] = keyword
     return render(request, 'admin/school.html', context)
